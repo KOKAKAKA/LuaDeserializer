8d5bbccb0be2c873508d5364e49837d1785b4462
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-11-27 19:41:22 +0700

    Update deserializer.rs

--- src/deserializer.rs
+++ src/deserializer.rs
@@ -37,27 +37,8 @@ impl Deserializer {
         );
 
         self.memory_stream.int_size = self.memory_stream.read_int8();
-        assert!(
-            self.memory_stream.int_size == 4 || self.memory_stream.int_size == 8,
-            "Invalid int size"
-        );
 
         self.memory_stream.size_t_size = self.memory_stream.read_int8();
-        assert!(
-            self.memory_stream.size_t_size == 4 || self.memory_stream.size_t_size == 8,
-            "Invalid size t size"
-        );
-
-        assert_eq!(
-            self.memory_stream.read_int8(),
-            4,
-            "Invalid instruction size"
-        );
-
-        //TODO: Float (4-byte) number support
-        assert_eq!(self.memory_stream.read_int8(), 8, "Invalid lua number size");
-
-        assert_eq!(self.memory_stream.read_int8(), 0, "Invalid integral flag");
 
         Chunk::new(&mut self.memory_stream)
     }
